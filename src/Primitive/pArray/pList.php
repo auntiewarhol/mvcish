8c417d8f0cab465d50b2a8fd5f7d89e084d045a2
JJ Wright <jj@plumflowersoftware.com>
2024-01-25 20:13:04 -0600

    fix

--- src/Primitive/pArray/pList.php
+++ src/Primitive/pArray/pList.php
@@ -4,7 +4,7 @@ namespace awPHP\MVCish\Primitive\pArray;
 class pList extends \awPHP\MVCish\Primitive\pArray {
 	public function __construct(array $value=null) {
 		if (!self::isList($value))
-			throw new \awPHP\Exception\ServerError("Cannot construct "
+			throw new \awPHP\MVCish\Exception\ServerError("Cannot construct "
 				.static::class." object with an Associative Array");
 		parent::__construct($value);
 	}
